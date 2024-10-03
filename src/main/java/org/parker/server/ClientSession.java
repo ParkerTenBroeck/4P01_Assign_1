b186e8c6b0e0de9c1159b1c14a40efe5e7a54900
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2024-10-03 18:45:47 -0400

    refactored ClientSession to pass TooManyAttempts test

--- src/main/java/org/parker/server/ClientSession.java
+++ src/main/java/org/parker/server/ClientSession.java
@@ -3,11 +3,20 @@ package org.parker.server;
 import org.parker.Response;
 
 public class ClientSession {
+    private int unsuccessfulAttempts = 0;
     private final Server server;
     public ClientSession(Server server){
         this.server = server;
     }
     public Response authenticate(String username, String password){
-        return server.authenticate(username, password);
+        if (unsuccessfulAttempts >= 3){
+            return Response.SessionFailure.TooManyAttempts;
+        }
+        var response = server.authenticate(username, password);
+        unsuccessfulAttempts += 1;
+        if (response == Response.Success.Success){
+            unsuccessfulAttempts = 0;
+        }
+        return response;
     }
 }
