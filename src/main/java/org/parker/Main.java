976c713b47f274f096636ec89ded3f200e69677e
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2024-10-03 17:09:02 -0400

    initialized a java project

---
+++ src/main/java/org/parker/Main.java
@@ -0,0 +1,14 @@
+package org.parker;
+
+import org.junit.jupiter.api.Test;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Test
+    public void test(){
+    }
+}
